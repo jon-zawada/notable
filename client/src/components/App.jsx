5740b8af9b2a00f7fda496fadca25d7bbee4e900
jon-zawada <jonathan_zawada@yahoo.com>
2020-11-11 14:45:12 -0800

    working async get request chains to retreive physicians and appointments

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import axios from 'axios';
 
-const App = () => (
-  <div id="hello">Hello World From App</div>
-);
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      physicians: [],
+      displayedPatients: [],
+      currentPhysician: {}
+    }
+    this.getPhysicians = this.getPhysicians.bind(this);
+    this.getAppointments = this.getAppointments.bind(this);
+  }
+  componentDidMount() {
+    this.getPhysicians();
+  }
+
+  getPhysicians(){
+    axios.get('/physicians')
+      .then((res) => {
+        this.setState({physicians: res.data}, () => {
+          this.setState({currentPhysician: this.state.physicians[0]});
+        });
+      })
+      .then(() => {
+        this.getAppointments(this.state.currentPhysician.id);
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  };
+
+  getAppointments(id){
+    axios.get(`/physicians/${id}/patients`)
+      .then((res) => {
+        this.setState({displayedPatients: res.data});
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  }
+
+  render(){
+    return(
+      <div id="notable">
+        {/* <Doctors/>
+        <Appointments/> */}
+      </div>
+    )
+  }
+}
 
 export default App;
